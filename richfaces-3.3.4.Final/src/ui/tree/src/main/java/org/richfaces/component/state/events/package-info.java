4256a999ced7cb58b96e86f33a98ad0cebba3283
Giuliano Pintori <pintori@link.it>
2023-03-02 17:51:13 +0100

    Libreria RichFaces UI 3.3.4 Eliminati progetti non utilizzati:
    - Tree
    - TreeModel
    - TreeTable

--- richfaces-3.3.4.Final/src/ui/tree/src/main/java/org/richfaces/component/state/events/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides classes and interfaces describing state events
- */
-package org.richfaces.component.state.events;
--- richfaces-3.3.4.Final/src/ui/tree/src/main/java/org/richfaces/component/state/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides classes and interfaces describing component states
- */
-package org.richfaces.component.state;
