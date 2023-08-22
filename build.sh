3350987e478489af6d54389f1726da34788610f2
Andrea Poli <andreapoli80@gmail.com>
2023-08-22 17:12:40 +0200

    Corretti errori per utilizzo su tomcat10

--- build.sh
+++ build.sh
@@ -2,28 +2,40 @@ RELEASE=$1
 if [ -z "${RELEASE}" ]
 then
 	echo "Tipo di build non fornito"
-	echo "Error; usage: ./build.sh snapshot|release"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
 	exit 2
 fi
 if [ ! "snapshot" == "${RELEASE}" -a ! "release" == "${RELEASE}" ]
 then
 	echo "Tipo di build '${RELEASE}' sconosciuto"
-	echo "Error; usage: ./build.sh snapshot|release"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
 	exit 3
+fi
+
+UPLOAD=$2
+if [ -z "${UPLOAD}" ]
+then
+	UPLOAD=false
+fi
+if [ ! "${UPLOAD}" == "true" -a ! "${UPLOAD}" == "false" ]
+then
+	echo "Indicazione upload non valida '${UPLOAD}'"
+	echo "Error; usage: ./build.sh snapshot|release [upload:true/false]"
+	exit 4
 fi
 
 java -fullversion > out.txt 2>&1
 JAVA_VERSION=$(cat out.txt)
-RESULT=$(cat out.txt | grep "11")
+RESULT=$(cat out.txt | grep "17")
 rm -f out.txt
 
 if [ -z "${RESULT}" ]
 then
-	echo "Richiesta java version 11, trovata: ${JAVA_VERSION}"
+	echo "Richiesta java version 17, trovata: ${JAVA_VERSION}"
 fi
 
 find . -name target | xargs rm -rf
-export MAVEN_OPTS="-Xmx2048m -XX:MaxMetaspaceSize=2048m"
+export MAVEN_OPTS="-Xmx2048m -XX:MaxMetaspaceSize=2048m --add-opens=java.base/java.lang=ALL-UNNAMED"
 mvn install
 
 VERSION=$(ls package/target/govway-monitor-ui-*.jar  | cut -d '-' -f 4 | cut -d '.' -f 1)
@@ -32,5 +44,10 @@ then
 	echo "Versione libreria non trovata in package/target/govway-monitor-ui-*.jar"
 fi
 
-GROUP_ID="org.govway-monitor-ui"
-bash package/script/deploy.sh package/target/govway-monitor-ui-${VERSION}.jar govway-monitor-ui ${GROUP_ID} ${VERSION} ${RELEASE}
+if [ "UPLOAD" == "true" ]
+then
+	echo "Upload to maven repository ..."
+	GROUP_ID="org.govway-monitor-ui"
+	bash package/script/deploy.sh package/target/govway-monitor-ui-${VERSION}.jar govway-monitor-ui ${GROUP_ID} ${VERSION} ${RELEASE}
+	echo "Upload to maven repository finished"
+fi
---
+++ jsf-impl-1.2_15-06/src/src/main/java/com/sun/faces/el/SpringBeanFacesELResolver.java
@@ -0,0 +1,170 @@
+package com.sun.faces.el;
+
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.beans.FeatureDescriptor;
+import java.util.Iterator;
+
+import javax.faces.context.FacesContext;
+
+import jakarta.el.ELContext;
+import jakarta.el.ELException;
+import jakarta.el.ELResolver;
+import jakarta.el.PropertyNotWritableException;
+
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * JSF {@code ELResolver} that delegates to the Spring root {@code WebApplicationContext},
+ * resolving name references to Spring-defined beans.
+ *
+ * <p>Configure this resolver in your {@code faces-config.xml} file as follows:
+ *
+ * <pre class="code">
+ * &lt;application&gt;
+ *   ...
+ *   &lt;el-resolver&gt;org.springframework.web.jsf.el.SpringBeanFacesELResolver&lt;/el-resolver&gt;
+ * &lt;/application&gt;</pre>
+ *
+ * All your JSF expressions can then implicitly refer to the names of
+ * Spring-managed service layer beans, for example in property values of
+ * JSF-managed beans:
+ *
+ * <pre class="code">
+ * &lt;managed-bean&gt;
+ *   &lt;managed-bean-name&gt;myJsfManagedBean&lt;/managed-bean-name&gt;
+ *   &lt;managed-bean-class&gt;example.MyJsfManagedBean&lt;/managed-bean-class&gt;
+ *   &lt;managed-bean-scope&gt;session&lt;/managed-bean-scope&gt;
+ *   &lt;managed-property&gt;
+ *     &lt;property-name&gt;mySpringManagedBusinessObject&lt;/property-name&gt;
+ *     &lt;value&gt;#{mySpringManagedBusinessObject}&lt;/value&gt;
+ *   &lt;/managed-property&gt;
+ * &lt;/managed-bean&gt;</pre>
+ *
+ * with "mySpringManagedBusinessObject" defined as Spring bean in
+ * applicationContext.xml:
+ *
+ * <pre class="code">
+ * &lt;bean id="mySpringManagedBusinessObject" class="example.MySpringManagedBusinessObject"&gt;
+ *   ...
+ * &lt;/bean&gt;</pre>
+ *
+ * @author Juergen Hoeller
+ * @since 2.5
+ * @see WebApplicationContextFacesELResolver
+ */
+public class SpringBeanFacesELResolver extends ELResolver {
+
+	@Override
+	@Nullable
+	public Object getValue(ELContext elContext, @Nullable Object base, Object property) throws ELException {
+		if (base == null) {
+			String beanName = property.toString();
+			WebApplicationContext wac = getWebApplicationContext(elContext);
+			if (wac.containsBean(beanName)) {
+				elContext.setPropertyResolved(true);
+				return wac.getBean(beanName);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	@Nullable
+	public Class<?> getType(ELContext elContext, @Nullable Object base, Object property) throws ELException {
+		if (base == null) {
+			String beanName = property.toString();
+			WebApplicationContext wac = getWebApplicationContext(elContext);
+			if (wac.containsBean(beanName)) {
+				elContext.setPropertyResolved(true);
+				return wac.getType(beanName);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void setValue(ELContext elContext, @Nullable Object base, Object property, Object value) throws ELException {
+		if (base == null) {
+			String beanName = property.toString();
+			WebApplicationContext wac = getWebApplicationContext(elContext);
+			if (wac.containsBean(beanName)) {
+				if (value == wac.getBean(beanName)) {
+					// Setting the bean reference to the same value is alright - can simply be ignored...
+					elContext.setPropertyResolved(true);
+				}
+				else {
+					throw new PropertyNotWritableException(
+							"Variable '" + beanName + "' refers to a Spring bean which by definition is not writable");
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean isReadOnly(ELContext elContext, @Nullable Object base, Object property) throws ELException {
+		if (base == null) {
+			String beanName = property.toString();
+			WebApplicationContext wac = getWebApplicationContext(elContext);
+			if (wac.containsBean(beanName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	@Nullable
+	public Iterator<FeatureDescriptor> getFeatureDescriptors(ELContext elContext, @Nullable Object base) {
+		return null;
+	}
+
+	@Override
+	public Class<?> getCommonPropertyType(ELContext elContext, @Nullable Object base) {
+		return Object.class;
+	}
+
+	/**
+	 * Retrieve the web application context to delegate bean name resolution to.
+	 * <p>The default implementation delegates to FacesContextUtils.
+	 * @param elContext the current JSF ELContext
+	 * @return the Spring web application context (never {@code null})
+	 */
+	protected WebApplicationContext getWebApplicationContext(ELContext elContext) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+//		return FacesContextUtils.getRequiredWebApplicationContext(facesContext);
+		Assert.notNull(facesContext, "FacesContext must not be null");
+		Object attr = facesContext.getExternalContext().getApplicationMap().get(
+				WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		if (attr == null) {
+			return null;
+		}
+		if (attr instanceof RuntimeException) {
+			throw (RuntimeException) attr;
+		}
+		if (attr instanceof Error)  {
+			throw (Error) attr;
+		}
+		if (!(attr instanceof WebApplicationContext)) {
+			throw new IllegalStateException("Root context attribute is not of type WebApplicationContext: " + attr);
+		}
+		return (WebApplicationContext) attr;
+	}
+
+}
